a4cf65594c2a27f34c73d47411f3722d9d8ce800
Rahul Jain <rahulj51@gmail.com>
2022-12-19 17:40:43 +0100

    Day 2

---
+++ solutions/models/day2/cleaner.sql
@@ -0,0 +1,50 @@
+{{
+    config (
+        schema = "day2"
+    )
+}}
+
+with name_array as (
+    select
+        customerid,
+        name,
+        phone,
+        string_to_array(name, ' ') as name_as_array
+    from {{ ref('customers') }}
+),
+
+initials as (
+    select
+        customerid,
+        name,
+        phone,
+        array_agg(substring(str, 1,1)) as initials
+
+    from (select *, unnest(name_as_array) as str from name_array) as a
+
+    group by customerid, name, phone
+
+),
+
+candidates as (
+    select
+        customerid,
+        name,
+        phone
+    from initials
+    where array_to_string(initials, '') = 'JD'
+)
+
+select
+    c.customerid,
+    c.name,
+    c.phone,
+    o.orderid,
+    array_agg(p.description) as items
+from {{ ref('orders') }} o
+    join candidates c on o.customerid = c.customerid
+    join {{ ref('orders_items') }} oi on o.orderid = oi.orderid
+    join {{ ref('products') }} p on oi.sku = p.sku
+where date_part('year', ordered_at) = 2017
+and lower(description) ~* 'coffee|bagel'
+group by c.customerid, c.name, c.phone, o.orderid
